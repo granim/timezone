15e6762e0b85d7f37aa3c8a5f812e5355d7d30ef
Grant Murphy <grantsmurphy@gmail.com>
2019-06-04 09:19:38 -0400

    added spring security configuration

--- src/main/java/com/timezone/demo/config/SecurityConfiguration.java
+++ src/main/java/com/timezone/demo/config/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.timezone.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +17,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth
                .inMemoryAuthentication()
-               .withUser("admin").password("password").roles("ADMIN")
+               .withUser("cc").password(passwordEncoder().encode("pass")).roles("ADMIN")
                .and()
-               .withUser("grant").password("grant123").roles("USER");
+               .withUser("cd").password(passwordEncoder().encode("pass")).roles("USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers( "/workers/**", "/coworkers/**", "/clients/**", "/fragments/**").authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
