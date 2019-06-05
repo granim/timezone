e0abd3528d5e5c81787617c4084752c957c31c24
Grant Murphy <grantsmurphy@gmail.com>
2019-06-05 13:19:02 -0400

    configure Spring Security logout action

--- src/main/java/com/timezone/demo/config/SecurityConfiguration.java
+++ src/main/java/com/timezone/demo/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -50,7 +51,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 //Replace basic with form based Auth
                 /*.httpBasic();*/
                 .formLogin()
-                .loginPage("/login").permitAll();
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
 
     }
 
