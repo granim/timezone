1477872ce5060e72a3b11d48a90f97c49134900a
Grant Murphy <grantsmurphy@gmail.com>
2019-06-05 14:49:46 -0400

    add Spring Security Thymeleaf integration

--- src/main/java/com/timezone/demo/config/SecurityConfiguration.java
+++ src/main/java/com/timezone/demo/config/SecurityConfiguration.java
@@ -55,7 +55,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
                 .and()
-                .rememberMe();
+                .rememberMe()
+                 ;
 
 
     }
--- src/main/java/com/timezone/demo/services/CrudService.java
+++ src/main/java/com/timezone/demo/services/CrudService.java
@@ -1,9 +1,13 @@
 package com.timezone.demo.services;
 
+
+
+import org.springframework.security.access.annotation.Secured;
+
 import java.util.Set;
 
 public interface CrudService<T, ID> {
-
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     Set<T> findAll();
 
     T findById(ID id);
