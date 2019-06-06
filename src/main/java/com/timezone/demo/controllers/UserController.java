e5896ef7c68c7d836f4d3e3dd41f2f1e8c157ee3
Grant Murphy <grantsmurphy@gmail.com>
2019-06-06 00:41:44 -0400

    implement user controller

--- src/main/java/com/timezone/demo/controllers/UserController.java
+++ src/main/java/com/timezone/demo/controllers/UserController.java
@@ -5,15 +5,18 @@ import com.timezone.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @RequestMapping("/users")
 @Controller
 public class UserController {
 
-    private static final String VIEWS_USER_CREATE_OR_UPDATE_FORM = "/users/createOrUpdateWorkerForm";
+    private static final String VIEWS_USER_CREATE_OR_UPDATE_FORM = "/users/createOrUpdateUserForm";
     private final UserService userService;
 
     @Autowired
@@ -32,6 +35,44 @@ public class UserController {
         return "users/findUsers";
     }
 
+    @GetMapping("/{userId}")
+    public ModelAndView showBaseUser(@PathVariable("workerId") Long userId) {
+        ModelAndView mav = new ModelAndView("users/baseUserDetails");
+        mav.addObject(userService.findById(userId));
+        return mav;
+    }
 
+    @GetMapping("/new")
+    public String initCreationForm(Model model) {
+        model.addAttribute("worker", User.builder().build());
+        return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid User user, BindingResult result){
+        if(result.hasErrors()) {
+            return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+        } else {
+            User savedUser = userService.save(user);
+            return "redirect:/users/" + savedUser.getId();
+        }
+    }
+
+    @GetMapping("/{userId}/edit")
+    public String initUpdateUserForm(@PathVariable Long userId, Model model){
+        model.addAttribute(userService.findById(userId));
+        return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/{userId}/edit")
+    public String processUpdateUserForm(@Valid User user, BindingResult result, @PathVariable Long userId){
+        if(result.hasErrors()){
+            return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+        } else {
+            user.setId(userId);
+            User savedUser = userService.save(user);
+            return "redirect:/users/" + savedUser.getId();
+        }
+    }
 
 }
--- src/main/java/com/timezone/demo/model/User.java
+++ src/main/java/com/timezone/demo/model/User.java
@@ -93,5 +93,11 @@ public class User{
         return new ArrayList<>();
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
