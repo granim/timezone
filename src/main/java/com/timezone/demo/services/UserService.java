f59fd18173ce395aae509a53963ca8745ac4d4ee
Grant Murphy <grantsmurphy@gmail.com>
2019-06-10 09:22:04 -0400

    added UserRegistration Controller

--- src/main/java/com/timezone/demo/services/UserService.java
+++ src/main/java/com/timezone/demo/services/UserService.java
@@ -1,11 +1,14 @@
 package com.timezone.demo.services;
 
+import com.timezone.demo.dto.UserRegistrationDto;
 import com.timezone.demo.model.User;
 
 public interface UserService extends CrudService<User, Long>{
 
         User findByUserName(String username);
 
+       User findByEmail(String email);
 
+       User save(UserRegistrationDto registration);
 
 }
